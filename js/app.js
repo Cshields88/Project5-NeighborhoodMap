de51cdc6d605425a66d0e189aa33f686ba082a26
chris <chrisshields88@gmail.com>
2016-02-04 19:20:49 -0500

    update

--- js/app.js
+++ js/app.js
@@ -11,121 +11,41 @@
             zoom: 8
         })
     };
+      initMap();
 
 
-        var MarkerViewModel = function () {
 
-            function markerDisplay() {
-                for (var i = 0; i < locations.length; i++) {
-                    var latlng = new google.maps.LatLng(locations[i].lat, locations[i].lng);
-                    var name = locations[i].name;
-                    createMarker(latlng, name);
-                }
-            };
+        function MarkerViewModel () {
+
+  //Locations Array holding Marker Properties
+          var locations = ko.observableArray([
+            {name: 'Founders Brewing Co.', lat: 42.9584, lng: -85.6742},
+            {name: 'Bells Brewery', lat: 42.285110, lng: -85.449972},
+            {name: 'New Holland Brewing Co.', lat: 42.790067, lng: -86.104204},
+            {name: 'Shorts Brewing Co.', lat: 44.976315, lng: -85.210035},
+            {name: 'Dark Horse Brewing Co.', lat: 42.266260, lng: -84.963444},
+            {name: 'Atwater Brewery', lat: 42.337311, lng: -83.018083}
+          ])
+
+          function markerDisplay() {
+            for (var i = 0; i < locations().length; i++) {
+              var latlng = new google.maps.LatLng(locations()[i].lat, locations()[i].lng);
+              var name = locations()[i].name;
+              createMarker(latlng, name);
+            }
+          };
 
 
         function createMarker(latlng, name) {
             var marker = new google.maps.Marker({
                 map: map,
                 position: latlng,
-                title: name
+                title: name,
+                icon: 'icons/beer.png'
             })
         };
+      //  markerDisplay();
 
-        markerDisplay();
-        //Locations Array holding Marker Properties
-        var locations = ko.observableArray([
-        //Location 1
-            {
-                name: 'Founders Brewing Co.',
-                position: {
-                    lat: 42.9584,
-                    lng: -85.6742
-                },
-                icon: 'icons/beer.png'
-            },
-        //Location 2
-            {
-                name: 'Bells Brewery',
-                position: {
-                    lat: 42.285110,
-                    lng: -85.449972
-                },
-                icon: 'icons/beer.png'
-            },
-        //Location 3
-            {
-                name: 'New Holland Brewing Co.',
-                position: {
-                    lat: 42.790067,
-                    lng: -86.104204
-                },
-                icon: 'icons/beer.png'
-            },
-        //Location 4
-            {
-                name: 'Shorts Brewing Co.',
-                position: {
-                    lat: 44.976315,
-                    lng: -85.210035
-                },
-                icon: 'icons/beer.png'
-        },
-        //Location 5
-            {
-                name: 'Dark Horse Brewing Co.',
-                position: {
-                    lat: 42.266260,
-                    lng: -84.963444
-                },
-                icon: 'icons/beer.png'
-            },
-        //Location 6
-            {
-                name: 'Atwater Brewery',
-                position: {
-                    lat: 42.337311,
-                    lng: -83.018083
-                },
-                icon: 'icons/beer.png'
-            }
-        // //Location 7
-        //     {
-        //         name: 'Arbor Brewing Co.',
-        //         position: {
-        //             lat: 42.250136,
-        //             lng: -83.609713,
-        //         },
-        //         icon: 'icons/beer.png'
-        //     },
-        // //Location 8
-        //     {
-        //         name: 'Arcadia Brewing Co.',
-        //         position: {
-        //             lat: 42.295278,
-        //             lng: -85.570512
-        //         },
-        //         icon: 'icons/beer.png'
-        // },
-        // //Location 9
-        //     {
-        //         name: 'Right Brain Brewery',
-        //         position: {
-        //             lat: 44.749605,
-        //             lng: -85.619070
-        //         },
-        //         icon: 'icons/beer.png'
-        // },
-        // //Location 10
-        //     {
-        //         name: 'Original Gravity Brewing Co.',
-        //         position: {
-        //             lat: 42.083974,
-        //             lng: -83.674940
-        //         },
-        //         icon: 'icons/beer.png'
-        // }
 
-        ]);
-    };
-        ko.applyBindings(new MarkerViewModel());
+        };
+        ko.applyBindings(MarkerViewModel());
--- js/snippets.js
+++ js/snippets.js
@@ -120,3 +120,98 @@ for (i = 0; i < self.locations.length; i++) {
 }(marker, i));
 }
 
+
+
+var locations = ko.observableArray([
+//Location 1
+    {
+        name: 'Founders Brewing Co.',
+        position: {
+            lat: 42.9584,
+            lng: -85.6742
+        },
+        icon: 'icons/beer.png'
+    },
+//Location 2
+    {
+        name: 'Bells Brewery',
+        position: {
+            lat: 42.285110,
+            lng: -85.449972
+        },
+        icon: 'icons/beer.png'
+    },
+//Location 3
+    {
+        name: 'New Holland Brewing Co.',
+        position: {
+            lat: 42.790067,
+            lng: -86.104204
+        },
+        icon: 'icons/beer.png'
+    },
+//Location 4
+    {
+        name: 'Shorts Brewing Co.',
+        position: {
+            lat: 44.976315,
+            lng: -85.210035
+        },
+        icon: 'icons/beer.png'
+},
+//Location 5
+    {
+        name: 'Dark Horse Brewing Co.',
+        position: {
+            lat: 42.266260,
+            lng: -84.963444
+        },
+        icon: 'icons/beer.png'
+    },
+//Location 6
+    {
+        name: 'Atwater Brewery',
+        position: {
+            lat: 42.337311,
+            lng: -83.018083
+        },
+        icon: 'icons/beer.png'
+    },
+//Location 7
+    {
+        name: 'Arbor Brewing Co.',
+        position: {
+            lat: 42.250136,
+            lng: -83.609713,
+        },
+        icon: 'icons/beer.png'
+    },
+//Location 8
+    {
+        name: 'Arcadia Brewing Co.',
+        position: {
+            lat: 42.295278,
+            lng: -85.570512
+        },
+        icon: 'icons/beer.png'
+},
+//Location 9
+    {
+        name: 'Right Brain Brewery',
+        position: {
+            lat: 44.749605,
+            lng: -85.619070
+        },
+        icon: 'icons/beer.png'
+},
+//Location 10
+    {
+        name: 'Original Gravity Brewing Co.',
+        position: {
+            lat: 42.083974,
+            lng: -83.674940
+        },
+        icon: 'icons/beer.png'
+}
+
+]);
