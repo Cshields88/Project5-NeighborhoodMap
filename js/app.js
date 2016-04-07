412bd384d2d3cfb610ec1b200af5694e8ebce462
chris <chrisshields88@gmail.com>
2016-04-06 20:46:47 -0400

    Update
    added list linking and search functionality. Also updated Readme

--- js/app.js
+++ js/app.js
@@ -1,191 +1,206 @@
 //Initialize Map
-var map, mapDiv;
+var map, marker;
 
 
 function initMap() {
-    mapDiv = document.getElementById('map-canvas');
-    map = new google.maps.Map(mapDiv, {
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), { //Map Data
         center: {
-            lat: 43.5978,
+            lat: 45.5978,
             lng: -84.7675
         },
-        zoom: 7,
+        zoom: 6,
         draggable: false,
         scrollwheel: false
-    })
+    });
 
-};
-
-initMap();
-
-
-locations = [
-    {
-        name: 'Founders Brewing Co.',
-        lat: 42.9584,
-        lng: -85.6742
+    var locations = [ //Location Data
+        {
+            name: 'Founders Brewing Co.',
+            lat: 42.9584,
+            lng: -85.6742,
+            url: 'http://www.foundersbrewing.com'
     },
-    {
-        name: 'Bells Brewery',
-        lat: 42.285110,
-        lng: -85.449972
+        {
+            name: 'Bells Brewery',
+            lat: 42.285110,
+            lng: -85.449972,
+            url: 'http://www.bellsbeer.com'
     },
-    {
-        name: 'New Holland Brewing Co.',
-        lat: 42.790067,
-        lng: -86.104204
+        {
+            name: 'New Holland Brewing Co.',
+            lat: 42.790067,
+            lng: -86.104204,
+            url: 'http://www.newhollandbrew.com'
     },
-    {
-        name: 'Shorts Brewing Co.',
-        lat: 44.976315,
-        lng: -85.210035
+        {
+            name: 'Shorts Brewing Co.',
+            lat: 44.976315,
+            lng: -85.210035,
+            url: 'http://www.shortsbrewing.com'
     },
-    {
-        name: 'Dark Horse Brewery',
-        lat: 42.266260,
-        lng: -84.963444
+        {
+            name: 'Dark Horse Brewery',
+            lat: 42.266260,
+            lng: -84.963444,
+            url: 'http://www.darkhorsebrewery.com'
     },
-    {
-        name: 'Kuhnhenn Brewing Company',
-        lat: 42.527963,
-        lng: -83.046753
+        {
+            name: 'Kuhnhenn Brewing Company',
+            lat: 42.527963,
+            lng: -83.046753,
+            url: 'http://www.kbrewery.com'
     },
-    {
-        name: 'Jolly Pumpkin Artisan Ales',
-        lat: 42.323838,
-        lng: -83.878447
+        {
+            name: 'Jolly Pumpkin Artisan Ales',
+            lat: 42.323838,
+            lng: -83.878447,
+            url: 'http://www.jollypumpkin.com'
     },
-    {
-        name: 'Arcadia Brewing Co.',
-        lat: 42.295278,
-        lng: -85.570512
+        {
+            name: 'Arcadia Brewing Co.',
+            lat: 42.295278,
+            lng: -85.570512,
+            url: 'http://www.arcadiaales.com'
     },
-    {
-        name: 'Detroit Beer Company',
-        lat: 42.336136,
-        lng: -83.048882
+        {
+            name: 'Detroit Beer Company',
+            lat: 42.336136,
+            lng: -83.048882,
+            url: 'http://www.detroitbeerco.com'
     },
-    {
-        name: 'North Peak Brewing Company',
-        lat: 44.765242,
-        lng: -85.628562
+        {
+            name: 'North Peak Brewing Company',
+            lat: 44.765242,
+            lng: -85.628562,
+            url: 'http://www.northpeak.net'
     }
 ];
 
 
 
-function ViewModel() {
-    var self = this;
+    function ViewModel() {
+        var self = this;
 
-    function placeInfo(data) {
-        self.name = ko.observable(data.name);
-        self.lat = ko.observable(data.lat);
-        self.lng = ko.observable(data.lng);
-        self.LatLng = ko.computed(function () {
-            return self.lat() + self.lng;
-        })
-    };
+        function placeInfo(data) { //Knockout Obsrvables
+            self.name = ko.observable(data.name);
+            self.url = ko.obsrvable(data.url);
+            self.lat = ko.observable(data.lat);
+            self.lng = ko.observable(data.lng);
+            self.LatLng = ko.computed(function () {
+                return self.lat() + self.lng();
+            })
+        };
 
-    var infowindow; //Creates instance of infowindow
-    var content;
-
-    self.allPlaces = ko.observableArray(locations);
-
+        var infowindow; //Creates instance of infowindow
+        var content; //Creates instance of Content;
 
 
-    // self.makeMarkers = function () {
-    //        locations.forEach(function (loc) {
-    //            self.allPlaces.push(new placeInfo(loc));
-    //        });
 
-    self.allPlaces().forEach(function (place) {
-        place.marker = new google.maps.Marker({
-            map: map,
-            position: new google.maps.LatLng(place.lat, place.lng),
-            title: place.name,
-            animation: google.maps.Animation.DROP,
-            icon: 'icons/beer.png',
-        });
-        var infoNames = place.name;
-        var urlNames = encodeURI(place.name);
-        //Wrapped the event listeners in an anonymous function to work as a closure, allowing only one infowindow to be opened at a time.
-        google.maps.event.addListener(place.marker, 'click', function () {
-            //if statement checks to see if an infowindow exists, if not it will create one
-            if (!infowindow) {
-                infowindow = new google.maps.InfoWindow();
-            }
+        self.allPlaces = ko.observableArray(locations); //Links location data to an observable
 
-            infowindow.open(map, place.marker);
-
-
-            //Wikipedia API
-
-            var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + urlNames + "&limit=1&redirects=return&format=json"
-
-            $.ajax({
-                url: wikiUrl,
-                dataType: "jsonp",
-                success: function (response) {
-                    var articleList = response[1];
-                    console.log(response);
-
-                    if (articleList.length > 0) {
-                        for (var i = 0; i < articleList.length; i++) {
-                            var articleStr = articleList[i];
-                            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                            content = '<div class="info">' + '<h4>' + infoNames + '</h4>' + '<p>' + response[2] + '</p>' + '<p>' + response[3] + '</p>' + '</div>';
-                            infowindow.setContent(content);
-
-                        };
-                    } else {
-                        content = '<div class="info">' + '<h4>' + infoNames + '</h4>' + '<p>' + "Sorry, No Articles Found on Wikipedia" + '</p>' + '</div>';
-                        infowindow.setContent(content);
-                    }
-
-                },
-                error: (function () {
-                    content = '<div class="info">' + '<h4>' + infoNames + '</h4>' + '<p>' + "Failed to reach Wikipedia Servers, please try again" + '</p>' + '</div>';
-                    infowindow.setContent(content);
-                })
+        self.allPlaces().forEach(function (place) { //Creates markers and infowindows for each location on the map
+            marker = new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(place.lat, place.lng),
+                title: place.name,
+                animation: google.maps.Animation.DROP,
+                icon: 'icons/beer.png',
             });
+            place.marker = marker;
+            google.maps.event.addListener(place.marker, 'click', function () { //opens and bounces infowindow when marker is clicked.
+                if (!infowindow) {
+                    infowindow = new google.maps.InfoWindow();
+                }
 
-            console.log(wikiUrl);
-
-        });
-        google.maps.event.addListener(place.marker, 'click', function () {
-            if (place.marker.getAnimation() !== null) {
-                place.marker.setAnimation(null);
-            } else {
+                infowindow.open(map, place.marker);
+                iContent = '<h3 class="text-center" id="infoTitle">' + place.name + '</h3>' + '<p class="text-center">' + place.url + '</p>' + '<h5>' + 'Please Click Name On List For More Information' + '</h5>'
+                infowindow.setContent(iContent);
                 place.marker.setAnimation(google.maps.Animation.BOUNCE);
                 setTimeout(function () {
                     place.marker.setAnimation(null);
-                }, 1200);
+                }, 1500);
+            });
+        }); //End ForEach
+
+        //Populates Infowindows with API information, and is also linked to Click Event Binding
+
+        self.list = function (place) {
+            var marker;
+            for (var i = 0; i < locations.length; i++) {
+                if (locations[i].name == place.name) {
+                    marker = place.marker[i];
+                    place.marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function () {
+                        place.marker.setAnimation(null);
+                    }, 1500);
+                    if (!infowindow) {
+                        infowindow = new google.maps.InfoWindow();
+                    }
+
+                    infowindow.open(map, place.marker);
+
+
+                    //Wikipedia API
+
+                    var infoNames = place.name;
+                    var urlNames = encodeURI(place.name);
+
+                    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + urlNames + "&limit=1&redirects=return&format=json";
+
+                    $.ajax({
+                        url: wikiUrl,
+                        dataType: "jsonp",
+                        success: function (response) {
+                            var articleList = response[1];
+                            console.log(response);
+
+                            if (articleList.length > 0) {
+                                for (var i = 0; i < articleList.length; i++) {
+                                    var articleStr = articleList[i];
+                                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                                    content = '<div class="info">' + '<h3 class="text-center" id="infoTitle">' + infoNames + '</h3>' + '<p id="info">' + response[2] + '</p>' + '<p class="text-center">' + response[3] + '</p>' + '</div>';
+                                    infowindow.setContent(content);
+
+                                };
+                            } else {
+                                content = '<div class="info">' + '<h3 class="text-center" id="infoTitle">' + infoNames + '</h3>' + '<p id="info">' + "Sorry, No Articles Found on Wikipedia" + '</p>' + '</div>';
+                                infowindow.setContent(content);
+                            }
+
+                        },
+                        error: (function () {
+                            content = '<div class="info">' + '<h3 class="text-center" id="infoTitle">' + infoNames + '</h3>' + '<p id="info">' + "Failed to reach Wikipedia Servers, please try again" + '</p>' + '</div>';
+                            infowindow.setContent(content);
+                        })
+                    });
+                }
             }
-        });
+        };
+        // Search functionality on location names
+        self.query = ko.observable(''); //Creates an observable for the search bar
 
-        self.list = ko.observable(function () {
+        self.search = ko.computed(function () {
+            ko.utils.arrayFilter(self.allPlaces(), function (place) {
 
-        });
-    }); //End ForEach
+                var filter = place.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0; //Filters Location Markers on map.
 
-
-
-    // };
-    //self.makeMarkers();
-
-    //Search Filtering
-    this.search = ko.observable("");
-
-
-    // var infoNames = place.name;
-
-
-
+                if (filter === true) { //if letters typed match any letters of location names show results
+                    place.marker.setVisible(true);
+                    return filter;
+                } else { //else hide all non-matching markers
+                    place.marker.setVisible(false);
+                    return filter;
+                }
+            });
+        }, self);
 
 
 
 
-}; //ViewModel End
 
 
-ko.applyBindings(new ViewModel());
+    }; //ViewModel End
+    ko.applyBindings(new ViewModel());
+
+}; //initMap End
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Udacity FEND Neighborhbood Map Project
+
+##### This Application shows in my opinion the 10 best breweries Michigan has to offer.  It includes a search function, a clickable list, and a map of the locations.
+##### It also incorporates the Wikipedia API for more information on each brewery.
+
+
+## How To Use
+
+### Open the index.html file in a browser, or on a local server.
+
+##### When the list on the left is clicked, the infowindow will show information about each brewery from Wikipedia.
+##### If a marker is clicked an infowindow will display the name of the brewery and also the website url.
+##### To search for a specific brewery, you can type the name in the search bar and only the brewery you have searched for will be displayed.
+
+
+
+## Resources
+
+* Udacity Forums
+* Udacity 1 on 1 Meetings
+* Stackoverflow
+* YouTube
+ * https://www.youtube.com/watch?v=fEYx8dQr_cQ
+ * https://www.youtube.com/watch?v=_L4IQoAWD9E&list=PLgGbWId6zgaXFR4SW_3qJ55cxmEqRNIzx
+* Books
+ * O'Reilly KnockoutJS
+ * KnockoutJS Essentials
+ * KnockoutJS Web Development
+ * Google Maps JavaScript API Cookbook
+* http://knockoutjs.com/
+* http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
